8c456ecf4e4f3d408bde7a788949b369b3d2b1ed
meheraz1100 <meheraz1100@gmail.com>
2024-02-13 22:22:58 +0600

    max find in 3 and more

---
+++ max3.js
@@ -0,0 +1,24 @@
+const jim = 0;
+const tim = 0;
+const kim = 0;
+
+if(jim > tim && jim > kim) {
+    console.log("Jim is ultimate boss");
+}
+else if(tim > jim && tim > kim) {
+    console.log("Tim is ultimate boss");
+}
+else if(kim > jim && kim > tim) {
+    console.log("Kim is ultimate boss");
+}
+else{
+    console.log("No one is ultimate boss");
+}
+
+//task 
+function maxOfThree(num1, num2, num3) {
+    // write your code here
+}
+
+const max = Math.max(12, 1, 23,2, 323, 24,234, 23, 21, 78, 34);
+console.log("Max using Math.max : ", max);
